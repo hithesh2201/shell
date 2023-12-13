200d314cdd3a697fce0b474156ffc7f5c698a8bd
Hitesh <hithesh2201@gmail.com>
2023-12-13 19:10:56 +0530

    learning shell

---
+++ mysql_more_concised.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+check() {
+    if [ $? -ne 0 ]; then
+        echo "$1 not installed successfully"
+        exit 1
+    else
+        echo "$1 installed successfully"
+    fi
+}
+
+if [ $UID -ne 0 ]; then
+    echo "ERROR: Not a root user"
+    exit 1
+else
+    echo "You are a root user"
+fi
+
+yum install mysql git -y &>>temp.log
+check "MYSQL"
+check "GIT"
