f49072b716b51e2db67574e52277919c35ffc055
Hitesh <hithesh2201@gmail.com>
2023-12-18 19:22:22 +0530

    logs

--- delete_oldlogs.sh
+++ delete_oldlogs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 DIR="/tmp/shell_script_logs"
-
+CHECK() {
+    if [ $? -eq 0 ]
+    then
+        echo "$line successfully deleted"
+    fi
+}
 if [ ! -d $DIR ]
 then
     echo "we dont have that shell_script_logs directory "
@@ -13,4 +18,5 @@ LOGFILE=$(find $DIR -type f -mtime +14)
 while IFS= read -r line
 do
   rm -rf "$line"
+  CHECK
 done <<< "$LOGFILE"
