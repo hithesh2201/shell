71869e07f3a056ad47de1837881c8bf5e0825604
Hitesh <hithesh2201@gmail.com>
2023-12-19 13:19:57 +0530

    automation

--- create_instances.sh
+++ create_instances.sh
@@ -4,7 +4,14 @@ INSTANCE_TYPE=""
 
 for i in "${INSTANCES_NAME[@]}"
 do
-    echo "$i"
+    if [ $i = "mongodb" ] | [ $i = "mysql" ] |[ $i = "shipping" ]
+    then
+        INSTANCE_TYPE="t2.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    PRIVATE_IPADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE  --security-group-ids sg-0f0225d73ad9227cc)
+    echo "$i : $PRIVATE_IPADDRESS" 
 done
 
 # aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type $INSTANCE_TYPE  --security-group-ids sg-xxxxxxxx 
