a5e3b8a1cf048be43fcb1402080ac3119a6cbc0f
Hitesh <hithesh2201@gmail.com>
2023-12-19 13:13:58 +0530

    automation

--- create_instances.sh
+++ create_instances.sh
@@ -2,8 +2,9 @@
 INSTANCES_NAME=("mongodb" "mysql" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payments")
 INSTANCE_TYPE=""
 
-for i in INSTANCES_NAME[@]
+for i in "${INSTANCES_NAME[@]}"
 do
     echo "$i"
 done
+
 # aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type $INSTANCE_TYPE  --security-group-ids sg-xxxxxxxx 
