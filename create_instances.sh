0e6e80ad8a3aed5c2190e100dc6993844da039d7
Hitesh <hithesh2201@gmail.com>
2023-12-22 10:59:01 +0530

    replaced create

--- create_instances.sh
+++ create_instances.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-INSTANCES_NAME=("mongodb" "mysql" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payments")
+INSTANCES_NAME=("mongodb" "mysql" "redis" "catalogue" "user" "cart" "shipping")
 INSTANCE_TYPE=""
 DOMAIN_PATH="hiteshshop.online"
 HOSTED_ZONE_ID="Z0781952344MQ4MZCJ506"
@@ -20,6 +20,8 @@ do
                     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
                     --query 'Instances[0].PrivateIpAddress' \
                     --output text)
+    aws ec2 run-instances --image-id $AMI --instance-type 
+    $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
     echo "$i : $PRIVATE_IPADDRESS" 
     aws route53 change-resource-record-sets \
     --hosted-zone-id "$HOSTED_ZONE_ID" \
